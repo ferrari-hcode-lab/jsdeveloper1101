a5ba2a4e393eadeefb249796e49f836d2ec535f4
matananh <matananh@gmail.com>
2021-01-12 10:44:55 -0300

    iniciamos calendario

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./public/assets/scripts/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devtool: "eval-source-map",
+};
